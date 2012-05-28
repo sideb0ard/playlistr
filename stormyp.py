e5f0f7bc8bdb6208c027db38f456c5a2ee3887fd
Thorsten Sideb0ard <thorsten.sideb0ard@gmail.com>
2012-05-28 23:59:26 +0000

    backup

--- stormyp.py
+++ stormyp.py
@@ -3,40 +3,44 @@ import tornado.ioloop
 import tornado.web
 import redis
 import json
-from plistlib import readPlistFromString
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, tigrrr!!\n")
 
 class PlaylistHandler(tornado.web.RequestHandler):
-    def get(self, username = None, playlistID = None):
-        #self.write("Username: {0} // PlaylistID: {1}".format(username, playlistID))
+    def get(self, userName = None, playlistID = None):
+        #self.write("Username: {0} // PlaylistID: {1}\n".format(userName, playlistID))
         r = redis.StrictRedis(host='localhost', port=6379, db=0)
-        if playlistID is None:
-            #self.write("Printing all playlists\n")
-            key = "playlist:1:*"
+        if playlistID is None and userName is None:
+            self.write("Wow, something fuccccked up!\n")
+        elif playlistID is None:
+            #self.write("No Playlists - checking for all user playlists\n")
+            key = "playlist:user0:itunes:*"
             #print "KEY SI {0}".format(key)
-            allplaylistkeys = r.keys(key)
-            allplaylists = {}
-            for k in allplaylistkeys:
-                # NEED PAGINATION
+            alluserplaylistkeys = r.keys(key)
+            alluserplaylists = {}
+            for k in alluserplaylistkeys:
+                alluserplaylists[k] = r.get(k)
+            self.set_header("Content-Type", "application/json") 
+            #self.write("{0}".format(alluserplaylists))
+            self.write(alluserplaylists)
+        else:
+            #self.write("Username - {0} and playlist ID {1} \n".format(userName,playlistID))
+            #self.write("Playlists Bitch {0}\n".format(playlistID))
+            key = "playlist:user0:itunes:{0}:document".format(playlistID)
+            #self.write("Key -- {0}\n".format(key))
+            playlist = r.get(key)
+            self.set_header("Content-Type", "application/json") 
+            #self.write("Playlist -- {0}\n".format(playlist))
+            self.write(playlist)
+            
+        #        # NEED PAGINATION
                 #allplaylists { k : r.get(key) }
                 #self.write("{0}".format(r.get(k)))
-                #plist_version = readPlist(r.get(k))
-                plist_version = readPlistFromString(r.get(k))
-                print "plsit -- {0}".format(plist_version)
                 #allplaylists[k] = json.dumps(plist_version)
-                allplaylists[k] = r.get(k)
             #json_playlists = json.dumps(allplaylists)
-            print "{0}".format(allplaylists)
-        else:
-            self.write("Playlists Bitch {0}\n".format(playlistID))
-            key = "playlist:1:itunes:{0}:contents".format(playlistID)
-            self.write("Key -- {0}\n".format(key))
-            playlist = r.get(key)
-            self.write("Playlist -- {0}\n".format(playlist))
-            
+        #else:
 class TrackHandler(tornado.web.RequestHandler):
     def get(self,tid):
         r = redis.StrictRedis(host='localhost', port=6379, db=0)
@@ -58,11 +62,13 @@ class ImportPlaylistHandler(tornado.web.RequestHandler):
 
 application = tornado.web.Application([
     (r"/", MainHandler),
-    (r"/playlist/([a-zA-Z0-9]+)/?", PlaylistHandler),
-    (r"/playlist/([a-zA-Z0-9]+)/([0-9]+)", PlaylistHandler),
+    #(r"/playlist/?", PlaylistHandler),
+    #(r"/playlist/?([a-zA-Z0-9]+)?/?", PlaylistHandler),
+    #(r"/playlist/([a-zA-Z0-9]+)/?([0-9]+)?/?", PlaylistHandler),
+    (r"/([a-zA-Z0-9]+)/playlist/?([0-9]+)?/?", PlaylistHandler),
     #(r"/playlist/([0-9]+)", PlaylistHandler),
-    (r"/track/([0-9]+)", TrackHandler),
-    (r"/import/playlist/", ImportPlaylistHandler),
+    #(r"/track/([0-9]+)", TrackHandler),
+    #(r"/import/playlist/", ImportPlaylistHandler),
 ])
 
 if __name__ == "__main__":
